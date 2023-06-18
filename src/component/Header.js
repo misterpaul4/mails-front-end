f795dda841ae1d69c84176cc2699735aeb7c7cd7
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-06-18 20:47:31 +0300

    Fix build errors

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import homeIcon from '../img/home-icon.png';
-import NotifCounter from './NotificationCounter';
-import global from '../util/global';
-import preference from '../util/preference';
-import '../css/header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import homeIcon from "../img/home-icon.png";
+import NotifCounter from "./NotificationCounter";
+import global from "../util/global";
+import preference from "../util/preference";
+import "../css/header.css";
 
 const Header = () => {
   return (
-  <header className="App-header flexed-between">
-    <Link to='/'>
-      <img src={homeIcon} className='home-icon' />
-    </Link>
+    <header className="App-header flexed-between">
+      <Link to="/">
+        <img alt="icon" src={homeIcon} className="home-icon" />
+      </Link>
 
-    <div className='greet-user'>Hello, <span className='user'>{preference.currentUser}</span></div>
-
-    <Link to={global.inboxPath} className='mail-icon-container bg-img'>
-      <div className='notification-counter'>
-        <NotifCounter />
+      <div className="greet-user">
+        Hello, <span className="user">{preference.currentUser}</span>
       </div>
-    </Link>
-  </header>);
+
+      <Link to={global.inboxPath} className="mail-icon-container bg-img">
+        <div className="notification-counter">
+          <NotifCounter />
+        </div>
+      </Link>
+    </header>
+  );
 };
 
-export default Header
+export default Header;
--- src/pages/MessagePage.js
+++ src/pages/MessagePage.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../component/Header";
 import preference from "../util/preference";
 
@@ -7,37 +7,41 @@ const Message = () => {
   const [message, updateMessage] = useState();
 
   const currentPath = useLocation().pathname;
-  const messageID = currentPath.replace(`/users/${preference.currentUser}/messages/`, '');
+  const messageID = currentPath.replace(
+    `/users/${preference.currentUser}/messages/`,
+    ""
+  );
 
   useEffect(() => {
     fetch(`${preference.HOST}/messages/${messageID}`, {
       method: "GET",
       mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      }
-    }).then(response => {
-      if (response.status.toString() === '200') {
-        response.json().then(data => {
-          // action on data
-          updateMessage(data)
-        })
-      } else {
-        console.log('message not found');
-      }
-    }).catch(error => {
-      console.log('looks like there was a problem ', error);
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
+      .then((response) => {
+        if (response.status.toString() === "200") {
+          response.json().then((data) => {
+            // action on data
+            updateMessage(data);
+          });
+        } else {
+          console.log("message not found");
+        }
+      })
+      .catch((error) => {
+        console.log("looks like there was a problem ", error);
+      });
 
     return null;
-  }, []);
+  }, [messageID]);
 
-
-  return(
+  return (
     <>
-      {message ?
-          <div>
+      {message ? (
+        <div>
           <Header />
           <div className="message-bg">
             <div className="message-container">
@@ -46,13 +50,11 @@ const Message = () => {
             </div>
           </div>
         </div>
-        
-        :
+      ) : (
         <span>...</span>
-    }
+      )}
     </>
-
   );
-}
+};
 
 export default Message;
